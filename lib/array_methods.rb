3dbe977ac9d2675ce720dad1e2e277b34d1c32dd
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-16 03:54:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    if array[index] == value_to_find do
+    if array[index] == value_to_find
       return index
-    end
   end
   return nil
 end
